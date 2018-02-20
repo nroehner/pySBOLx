55772c1a023ac6c317b1a02565a55e756f2e6be7
nroehner <nicholasroehner@gmail.com>
2018-02-20 15:30:26 -0500

    Fixed addition of name property to extension classes so that it uses the Dublin Core namespace.

--- pySBOLx/pySBOLx.py
+++ pySBOLx/pySBOLx.py
@@ -9,12 +9,14 @@ OM_NS = 'http://www.ontology-of-units-of-measure.org/resource/om-2#'
 
 PROV_NS = 'http://www.w3.org/ns/prov#'
 
+DC_NS = 'http://purl.org/dc/terms/'
+
 class Implementation(TopLevel, PythonicInterface):
     
     def __init__(self, display_id='example', name=None, built=None, version='1'):
         TopLevel.__init__(self, SD2_NS + 'Implementation', display_id, version)
         if name is not None:
-            self.name = TextProperty(self.this, OM_NS + 'symbol', '0', '1', name)
+            self.name = TextProperty(self.this, DC_NS + 'title', '0', '1', name)
         if built is None:
             self.built = URIProperty(self.this, SD2_NS + 'built', '0', '1', built)
         else:
@@ -26,7 +28,7 @@ class Attachment(TopLevel, PythonicInterface):
     def __init__(self, display_id='example', name=None, source=None, attach_format=None, version='1'):
         TopLevel.__init__(self, SD2_NS + 'Attachment', display_id, version)
         if name is not None:
-            self.name = TextProperty(self.this, OM_NS + 'symbol', '0', '1', name)
+            self.name = TextProperty(self.this, DC_NS + 'title', '0', '1', name)
         if source is not None:
             self.source = URIProperty(self.this, SD2_NS + 'source', '0', '1', source)
         else:
@@ -42,7 +44,7 @@ class Experiment(TopLevel, PythonicInterface):
     def __init__(self, display_id='example', name=None, experimental_data=None, version='1'):
         TopLevel.__init__(self, SD2_NS + 'Experiment', display_id, version)
         if name is not None:
-            self.name = TextProperty(self.this, OM_NS + 'symbol', '0', '1', name)
+            self.name = TextProperty(self.this, DC_NS + 'title', '0', '1', name)
         if experimental_data is not None:
             self.experimentalData = URIProperty(self.this, SD2_NS + 'experimentalData', '0', '*', experimental_data)
         else:
@@ -54,7 +56,7 @@ class ExperimentalData(TopLevel, PythonicInterface):
     def __init__(self, display_id='example', name=None, version='1'):
         TopLevel.__init__(self, SD2_NS + 'ExperimentalData', display_id, version)
         if name is not None:
-            self.name = TextProperty(self.this, OM_NS + 'symbol', '0', '1', name)
+            self.name = TextProperty(self.this, DC_NS + 'title', '0', '1', name)
         self.register_extension_class(ExperimentalData, 'sd2')
 
 class Measure(Identified, PythonicInterface):
@@ -78,7 +80,7 @@ class Unit(TopLevel, PythonicInterface):
     def __init__(self, display_id='example', name=None, symbol=None, version='1'):
         TopLevel.__init__(self, OM_NS + 'Unit', display_id, version)
         if name is not None:
-            self.name = TextProperty(self.this, OM_NS + 'symbol', '0', '1', name)
+            self.name = TextProperty(self.this, DC_NS + 'title', '0', '1', name)
         if symbol is not None:
             self.symbol = TextProperty(self.this, OM_NS + 'symbol', '0', '1', symbol)
         else:
