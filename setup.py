a5e7799ed83b8a9794b7d52874c5cc70095189e2
nroehner <nicholasroehner@gmail.com>
2018-03-26 13:47:29 -0400

    Trying out new setup.py that changes requirements based on OS.

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from setuptools import setup
 
+install_requires=['rdflib']
+
+if sys.platform in {'win32'}:
+  install_requires.append('sbol')
+else:
+  install_requires.append('pysbol')
+
 setup(name='pySBOLx',
       version='0.1',
-      description='pysbol extended to support experimental data representation (built for 64-bit Windows and Python 3).',
+      description='pysbol extended to support experimental data representation.',
       url='https://github.com/SD2E/data-representation/sbol/pySBOLx',
       author='Nicholas Roehner',
       author_email='nicholasroehner@gmail.com',
       packages=['pySBOLx'],
-      install_requires=[
-          'rdflib',
-          'sbol'
+      install_requires=install_requires,
+      dependency_links=[
+        'git+https://git@github.com/nroehner/pySBOL_Win_64_3.git#egg=sbol'
       ],
-      dependency_links=['git+https://git@github.com/nroehner/pySBOL_Win_64_3.git#egg=sbol'],
       zip_safe=False)
